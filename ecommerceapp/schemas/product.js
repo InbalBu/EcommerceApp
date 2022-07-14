2bcf043b18ffa9e24aeb18f6239c2abafd8768de
InbalBu <inbalbu2@gmail.com>
2022-07-14 15:13:57 +0300

    sanity

---
+++ ecommerceapp/schemas/product.js
@@ -0,0 +1,32 @@
+export default {
+    name: 'product',
+    title: 'Product',
+    type: 'document',
+    fields: [
+        {
+            name: 'image',
+            title: 'Image',
+            type: 'array',
+            of: [{type: 'image'}],
+            options: {
+                hotspot: true,
+            }
+        },
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string',
+        },
+        {
+            name: 'price',
+            title: 'Price',
+            type: 'number',
+        },
+        {
+            name: 'description',
+            title: 'Description',
+            type: 'string',
+
+        }
+    ]
+}
