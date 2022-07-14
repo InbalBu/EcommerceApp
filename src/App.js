481afcd6151a0d951f47e56c5f07b481a75671da
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-14 19:18:57 +0300

    use effect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
 import React from "react";
 import { client } from './lib/client';
-
+import { useEffect,useState } from 'react';
 function App({products}) {
+
+const[storeProducts,setStoreProducts]=useState();
+
+useEffect(async () => {
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  localStorage.setItem("storeProducts",JSON.stringify(products));
+  setStoreProducts(JSON.stringify(products));
+  alert(storeProducts)
+},[])
+
+// useEffect(() => {
+//   const storeProducts= localStorage.getItem("storeProducts");
+//   setProducts(JSON.parse(storeProducts));
+// },product)
 
   return (
     <div className="App">
-    <h1>{products.name}</h1>
+    <h1>{products}</h1>
     </div>
    
   );
@@ -15,7 +30,7 @@ function App({products}) {
   export const getServerSideProps = async () => {
     const query = '*[_type == "product"]';
     const products = await client.fetch(query);
-  
+
     return {
       props: { products }
     }
