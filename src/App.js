38ab06b0b53594122286352f91a8da9a91bf8b8f
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-14 19:34:02 +0300

    added use effect to get product

--- src/App.js
+++ src/App.js
@@ -4,16 +4,45 @@ import { client } from './lib/client';
 import { useEffect,useState } from 'react';
 function App({products}) {
 
-const[storeProducts,setStoreProducts]=useState();
+const[storeProducts,setStoreProducts]=useState([]);
+const [singleProduct,setSingleProduct]=useState("");
+// useEffect(() => {
+//   const profileImg=JSON.parse(localStorage.getItem("profileImg"));
+//    const userFound=JSON.parse(localStorage.getItem("user"));
+//    if(profileImg!==null&&profileImg!==undefined&&profileImg!=={})setProfileImage(profileImg);
+//    setUser(userFound);
+//  const array=JSON.parse(localStorage.getItem('userMemories'));
+//  setMemoriesStart(Array.from(array).map((memorie)=>{
+//    setIndex(index+1);
+//    return <Grid item xs={12} md={6} lg={4}>
+//    <MemoryCard 
+//      ImgSrc={memorie.ImgSrc}
+//      Header={memorie.Header}
+//      Body={memorie.Body}
+//       Footer={memorie.Footer}
+//       id={memorie._id}
+//       key={index}
+//     onDelete={deleteMemory}
+//       /> 
+//    </Grid>
+//  }));
+// }, memoriesStart);
 
-useEffect(async () => {
-  const query = '*[_type == "product"]';
-  const products = await client.fetch(query);
-  localStorage.setItem("storeProducts",JSON.stringify(products));
-  setStoreProducts(JSON.stringify(products));
-  alert(storeProducts)
-},[])
+ useEffect(() => {
+   const getProducts = async () => {   
+     const query = '*[_type == "product"]';
+      const products = await client.fetch(query);
+      localStorage.setItem("storeProducts",JSON.stringify(products));
+       setStoreProducts(JSON.stringify(products));
+   }
+   getProducts();
+ }, [])
 
+function getName()
+{
+  const product=JSON.parse(localStorage.getItem("storeProducts"));
+  setSingleProduct(product[0].price);
+}
 // useEffect(() => {
 //   const storeProducts= localStorage.getItem("storeProducts");
 //   setProducts(JSON.parse(storeProducts));
@@ -21,19 +50,12 @@ useEffect(async () => {
 
   return (
     <div className="App">
-    <h1>{products}</h1>
+    <h1 onClick={getName}>hello</h1>
+    <h1>{singleProduct}</h1>
     </div>
    
   );
   }
 
-  export const getServerSideProps = async () => {
-    const query = '*[_type == "product"]';
-    const products = await client.fetch(query);
-
-    return {
-      props: { products }
-    }
-  }
 
 export default App;
