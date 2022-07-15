c8e622d850ef39cbe10f37cedba0bcad82d5eef5
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-15 13:48:20 +0300

    deleted commet

--- src/App.js
+++ src/App.js
@@ -5,28 +5,6 @@ import { useEffect,useState } from 'react';
 function App() {
 
 const[storeProducts,setStoreProducts]=useState([]);
-// useEffect(() => {
-//   const profileImg=JSON.parse(localStorage.getItem("profileImg"));
-//    const userFound=JSON.parse(localStorage.getItem("user"));
-//    if(profileImg!==null&&profileImg!==undefined&&profileImg!=={})setProfileImage(profileImg);
-//    setUser(userFound);
-//  const array=JSON.parse(localStorage.getItem('userMemories'));
-//  setMemoriesStart(Array.from(array).map((memorie)=>{
-//    setIndex(index+1);
-//    return <Grid item xs={12} md={6} lg={4}>
-//    <MemoryCard 
-//      ImgSrc={memorie.ImgSrc}
-//      Header={memorie.Header}
-//      Body={memorie.Body}
-//       Footer={memorie.Footer}
-//       id={memorie._id}
-//       key={index}
-//     onDelete={deleteMemory}
-//       /> 
-//    </Grid>
-//  }));
-// }, memoriesStart);
-
  useEffect(() => {
    const getProducts = async () => {   
      const query = '*[_type == "product"]';
