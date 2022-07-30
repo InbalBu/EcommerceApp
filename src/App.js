5bad9f9fb59dc41daeb01a92c931e27863cc5193
Nivbenaviv <96342824+Nivbenaviv@users.noreply.github.com>
2022-07-30 21:02:14 +0300

    changed cart

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from "react";
 import Profile from './Components/ProfilePage/Profile';
-import { useState } from 'react';
+import { useState,useEffect} from 'react';
 import Productpage from './Components/ProductPage/Productpage';
 import {Pay,SalesPage,ShoppingBagsPage,Homepage,RegisterPage,LoginPage,ShoesPage,SneakersPage,WalletPage,BackpackPage, ResetPassword} from "../src/pages/pages";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -10,10 +10,10 @@ import {
   Route
 } from "react-router-dom";
 export const StoreContext = React.createContext(); // to show on all app
-
 function App() {
 
 //#region constants
+
 const [subtotal, setSubtotal] =useState(0);
  const [cart,setCart]=useState([]);
  const [showCart,setshowCart]=useState(false);
@@ -21,6 +21,8 @@ const [subtotal, setSubtotal] =useState(0);
  const [user,setUser] =useState({ email:"",
  password:""});
 //#endregion
+
+
 
   return (
     <div className="App">
